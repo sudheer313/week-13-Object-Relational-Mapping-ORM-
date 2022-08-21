2332888ec0c8eb2485efe6ae5ca7365550dcfa1b
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-21 20:03:18 +1000

    updated routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,16 +5,35 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  try {
+
+    const data = await Category.findAll({
+      include: [Product]
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+
+    const data = await Category.findByPk(req.params.id, {
+      include: [Product]
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  // Category.create(req.body)
 });
 
 router.put('/:id', (req, res) => {
