1c946e4b817d44d27083c89ce5b719021962a4ac
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-26 08:45:57 +1000

    error in routs:

--- Develop/server.js
+++ Develop/server.js
@@ -14,3 +14,8 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+// ask BCS</br>
+// referred https://www.w3schools.com/mySQl/default.asp</br>
+// referred from https://github.com/kunalc156/E-Commerce</br>
+// referred from https://github.com/ashachakre0906/E-commerce-Backend-Application</br>
